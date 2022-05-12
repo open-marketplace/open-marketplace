125ddc8095ed210d0c0fde904e9ef85e58643e81
rafalmal <rafal.malenta@bitbag.pl>
2022-05-12 15:33:43 +0200

    Set class names and namespaces to requirements

--- src/DependencyInjection/MultiVendorMarketplaceExtension.php
+++ src/DependencyInjection/MultiVendorMarketplaceExtension.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace BitBag\MultiVendorMarketplace\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class MultiVendorMarketplaceExtension extends Extension
 {
     /**
      * @psalm-suppress UnusedVariable
