c8f5b4640362b7eaf1cb95add7ad6e78ff432f79
Jan Hickiewicz <jan.hickiewicz@bitbag.pl>
2022-05-16 11:08:08 +0200

    Make registered in services.xml AdminMainMenuListener and translations for it

---
+++ src/Menu/AdminMainMenuListener.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMainMenuListener
+{
+    public function addMarketplaceSubMenu(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $newSubmenu = $menu
+            ->addChild('marketplace')
+            ->setLabel('bitbag_sylius_mvm_plugin.ui.marketplace')
+        ;
+
+        $newSubmenu
+            ->addChild('vendors', [
+                'route' => 'sylius_admin_customer_index',
+            ])
+            ->setLabel('bitbag_sylius_mvm_plugin.ui.vendors')
+            ->setLabelAttribute('icon', 'users')
+        ;
+    }
+}
