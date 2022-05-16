08a8979c061d6d012b5b11f03038140318c3c184
Kamil_Widera <kamil.widera@bitbag.pl>
2022-05-16 12:16:54 +0200

    Created 'Marketplace' section in the admin panel and linked it to the product listings index page

---
+++ src/Menu/MultiVendorMarketplaceMenuListener.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class MultiVendorMarketplaceMenuListener
+{
+    public function buildMenu(MenuBuilderEvent $menuBuilderEvent): void
+    {
+        $menu = $menuBuilderEvent->getMenu();
+
+        $cmsRootMenuItem =
+            $menu
+                ->addChild('marketplace')
+                ->setLabel('bitbag_sylius_multi_vendor_marketplace_plugin.ui.marketplace');
+
+        $cmsRootMenuItem
+            ->addChild('multi_vendor_marketplace_product_listings', [
+                'route' => 'bitbag_sylius_multi_vendor_marketplace_plugin_admin_product_listing_index'
+            ])
+            ->setLabel('bitbag_sylius_multi_vendor_marketplace_plugin.ui.product_listings')
+            ->setLabelAttribute('icon', 'list');
+    }
+}
