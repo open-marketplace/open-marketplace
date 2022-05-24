5309ad63c77a3eb96b96fa511d22f4d41671082f
rafalmal <rafal.malenta@bitbag.pl>
2022-05-24 08:47:56 +0200

    prapared controller to be aware of exceptions thrown inside form

--- src/Controller/VendorController.php
+++ src/Controller/VendorController.php
@@ -16,6 +16,8 @@ use Sylius\Component\Resource\ResourceActions;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 
 class VendorController extends ResourceController
 {
@@ -24,74 +26,11 @@ class VendorController extends ResourceController
      */
     public function createAction(Request $request): Response
     {
-        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
-        $this->isGrantedOr403($configuration, ResourceActions::CREATE);
-        $newResource = $this->newResourceFactory->create($configuration, $this->factory);
-
-        $form = $this->resourceFormFactory->create($configuration, $newResource);
-        $form->handleRequest($request);
-
-        if ($request->isMethod('POST') && $form->isSubmitted() && $form->isValid()) {
-            $newResource = $form->getData();
-
-            $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::CREATE, $configuration, $newResource);
-
-            if ($event->isStopped() && !$configuration->isHtmlRequest()) {
-                throw new HttpException($event->getErrorCode(), $event->getMessage());
-            }
-            if ($event->isStopped()) {
-                $this->flashHelper->addFlashFromEvent($configuration, $event);
-
-                $eventResponse = $event->getResponse();
-                if (null !== $eventResponse) {
-                    return $eventResponse;
-                }
-
-                return $this->redirectHandler->redirectToIndex($configuration, $newResource);
-            }
-
-            if ($configuration->hasStateMachine()) {
-                $stateMachine = $this->getStateMachine();
-                $stateMachine->apply($configuration, $newResource);
-            }
-
-            $this->repository->add($newResource);
-
-            if ($configuration->isHtmlRequest()) {
-                $this->flashHelper->addSuccessFlash($configuration, ResourceActions::CREATE, $newResource);
-            }
-
-            $postEvent = $this->eventDispatcher->dispatchPostEvent(ResourceActions::CREATE, $configuration, $newResource);
-
-            if (!$configuration->isHtmlRequest()) {
-                return $this->createRestView($configuration, $newResource, Response::HTTP_CREATED);
-            }
-
-            $postEventResponse = $postEvent->getResponse();
-            if (null !== $postEventResponse) {
-                return $postEventResponse;
-            }
-
-            return $this->redirectHandler->redirectToResource($configuration, $newResource);
+        try {
+            return parent::createAction($request);
         }
-
-        if (!$configuration->isHtmlRequest()) {
-            return $this->createRestView($configuration, $form, Response::HTTP_BAD_REQUEST);
-        }
-
-        $initializeEvent = $this->eventDispatcher->dispatchInitializeEvent(ResourceActions::CREATE, $configuration, $newResource);
-        $initializeEventResponse = $initializeEvent->getResponse();
-        if (null !== $initializeEventResponse) {
-            return $initializeEventResponse;
-        }
-
-        return $this->render($configuration->getTemplate(ResourceActions::CREATE . '.html'), [
-            'configuration' => $configuration,
-            'metadata' => $this->metadata,
-            'resource' => $newResource,
-            $this->metadata->getName() => $newResource,
-            'form' => $form->createView(),
-        ]);
+        catch (\Exception $exception){
+            return $this->redirectToRoute('sylius_shop_login');
+        }       
     }
 }
