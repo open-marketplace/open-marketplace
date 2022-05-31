85b927573c595dc5af289e6ef5e526f83eb2bd35
rafalmal <rafal.malenta@bitbag.pl>
2022-05-31 09:03:44 +0200

    Created and registered VendorProvider service

---
+++ src/Service/VendorProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Service;
+
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\Customer;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\VendorInterface;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Exception\UserNotFoundException;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Symfony\Component\Security\Core\Security;
+
+class VendorProvider
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    
+    public function getLoggedVendor(): VendorInterface
+    {        
+        /** @var ShopUserInterface $user */
+        $user = $this->security->getUser();
+        if (null == $user) {
+            throw new UserNotFoundException();
+        }
+        /** @var Customer $customer */
+        $customer = $user->getCustomer();
+//        if (null == $customer) {
+//            return null;
+//        }
+        /** @var VendorInterface $vendor */
+        $vendor = $customer->getVendor();      
+        return $vendor;
+    }
+}
