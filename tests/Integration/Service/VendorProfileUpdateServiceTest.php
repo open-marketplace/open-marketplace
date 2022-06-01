6926d48fd470e6ba0b7dad59aac0c11b8512c8cf
rafalmal <rafal.malenta@bitbag.pl>
2022-06-01 12:28:36 +0200

    Created first phpunit test case

--- tests/Integration/Service/VendorProfileUpdateServiceTest.php
+++ tests/Integration/Service/VendorProfileUpdateServiceTest.php
@@ -2,20 +2,38 @@
 
 namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Integration\Service;
 
-//use Sylius\Tests\Api\JsonApiTestCase;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Nelmio\Alice\Loader\NativeLoader;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-use ApiTestCase\JsonApiTestCase;
-
-class VendorProfileUpdateServiceTest extends JsonApiTestCase
+class VendorProfileUpdateServiceTest extends WebTestCase
 {
-    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $this->dataFixturesPath = __DIR__.'DataFixtures';
-    }    
-   
-    public function test_it_loads_fixtures()
-    {
-//        $this->loadFixturesFromFile();
     }
+
+    public function test_phpUnitWorksFine()
+    {
+        self::bootKernel();
+        
+        $loader = new NativeLoader();
+        $manager = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $fixtures = $loader->loadFile(__DIR__.'/../DataFixtures/ORM/vendor_fixtures.yml')->getObjects();
+        foreach ($fixtures as $fixture){
+            $manager->persist($fixture);
+            $manager->flush();
+        }      
+        dd($fixtures);
+    }   
+    public function tearDown(): void
+    {
+//        self::bootKernel();
+        $purger = new ORMPurger(self::$kernel->getContainer()->get('doctrine')->getManager());
+        $purger->purge();        
+    }
+//    public function test_it_loads_fixtures()
+//    {
+////        $this->loadFixturesFromFile();
+//    }
 }
