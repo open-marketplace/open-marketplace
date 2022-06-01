90c22734c82c52d0dce48a8b2de34341f7702994
rafalmal <rafal.malenta@bitbag.pl>
2022-06-01 14:31:04 +0200

    upgraded webtestcase to jsonapitestcase

--- tests/Integration/Service/VendorProfileUpdateServiceTest.php
+++ tests/Integration/Service/VendorProfileUpdateServiceTest.php
@@ -2,33 +2,21 @@
 
 namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Integration\Service;
 
+use ApiTestCase\JsonApiTestCase;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\Vendor;
-use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\VendorAddress;
-use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Nelmio\Alice\Loader\NativeLoader;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class VendorProfileUpdateServiceTest extends WebTestCase
+class VendorProfileUpdateServiceTest extends JsonApiTestCase
 {
     public function __construct(?string $name = null, array $data = [], $dataName = '')
     {
-        parent::__construct($name, $data, $dataName);
+        parent::__construct($name, $data, $dataName);        
     }
 
-    public function test_phpUnitWorksFine()
+    public function test_phpUnitLoadsFixtures()
     {
-        self::bootKernel();
-        $loader = new NativeLoader();
-        $manager = self::$kernel->getContainer()->get('doctrine')->getManager();
-        $fixtures = $loader->loadFile(__DIR__.'/../DataFixtures/ORM/vendor_fixtures.yml')->getObjects();
-        foreach ($fixtures as $fixture){
-            $manager->persist($fixture);
-            $manager->flush();
-        }   
-    }
-    protected function tearDown(): void
-    {
-        $purger = new ORMPurger(self::$kernel->getContainer()->get('doctrine')->getManager());
-        $purger->purge();
+        $this->loadFixturesFromFile('vendor_fixtures.yml');
+        $manager = $this->getEntityManager();
+        $vendor = $manager->getRepository(Vendor::class)->findOneBy(['taxIdentifier'=>"1234567"]);
+        self::assertEquals('Test company name',$vendor->getCompanyName());     
     }
 }
