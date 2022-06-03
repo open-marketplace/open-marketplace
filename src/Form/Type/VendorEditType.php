6229c44db1f2cb53d6be11f6395644d0922a1320
Jan Hickiewicz <jan.hickiewicz@bitbag.pl>
2022-06-03 15:32:36 +0200

    Create vendor edit page skeleton

---
+++ src/Form/Type/VendorEditType.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusMultiVendorMarketplacePlugin\Form\Type;
+
+use BitBag\SyliusMultiVendorMarketplacePlugin\Form\VendorAddressType;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Valid;
+
+final class VendorEditType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('companyName', TextType::class, [
+                'label' => 'label',
+            ])
+            ->add('taxIdentifier', TextType::class, [
+                'label' => 'label',
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'label',
+            ])
+            ->add('vendorAddress', VendorAddressType::class, [
+                'label' => 'label',
+                'constraints' => [new Valid()],
+            ]);
+    }
+}
+
