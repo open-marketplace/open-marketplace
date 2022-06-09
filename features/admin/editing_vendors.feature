ffb81b4d9d15a80b09a7f917a1b5da6efdb75b3a
Jan Hickiewicz <jan.hickiewicz@bitbag.pl>
2022-06-09 11:31:00 +0200

    Refactor and fix behat test

--- features/admin/editing_vendors.feature
+++ features/admin/editing_vendors.feature
@@ -11,20 +11,33 @@ Feature: Editing Vendors
 
   @ui
   Scenario: Editing verified vendors which requested change of profile
-    Given There is a "verified" vendor which requested change
+    Given There is a "verified" vendor which "requested" change
     When I follow "Vendors"
-    And print current URL
-    And I click "Edit"
+    And I follow "Edit"
     Then I should see "Vendor requested changes"
     And I should see "Vendor details"
     And I should see "Vendor address"
 
   @ui
   Scenario: Editing verified vendors which did not requested change of profile
-    Given There is a "verified" vendor
+    Given There is a "verified" vendor which "did not requested" change
     When I follow "Vendors"
-    And I click "Edit"
+    And I follow "Edit"
     Then I should see "Vendor details"
     And I should see "Vendor address"
     And I should not see "Vendor requested changes"
+
+  @ui
+  Scenario: Editing unverified vendors which requested change of profile
+    Given There is a "unverified" vendor which "requested" change
+    When I follow "Vendors"
+    And I should not see "Edit"
+
+  @ui
+  Scenario: Editing unverified vendors which did not requested change of profile
+    Given There is a "unverified" vendor which "did not requested" change
+    When I follow "Vendors"
+    And I should not see "Edit"
+
+
 
--- tests/Behat/Context/Ui/Admin/VendorEditingContext.php
+++ tests/Behat/Context/Ui/Admin/VendorEditingContext.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
-use Behat\Mink\Element\DocumentElement;
 use Behat\MinkExtension\Context\RawMinkContext;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\Vendor;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,47 +20,17 @@ final class VendorEditingContext extends RawMinkContext implements Context
     }
 
     /**
-     * @Given There is a :ifVerified vendor
+     * @Given There is a :ifVerified vendor which :ifRequested change
      */
-    public function thereIsAVendor($ifVerified)
+    public function thereIsAVendorWhichChange($ifVerified, $ifRequested)
     {
         $vendor = new Vendor();
         $vendor->setCompanyName('vendor');
         $vendor->setTaxIdentifier('vendorTax');
         $vendor->setPhoneNumber('vendorPhone');
         $vendor->setStatus($ifVerified);
+        $ifRequested == 'requested' ? $vendor->setEditDate('editDate') : $vendor->setEditDate(null);
         $this->entityManager->persist($vendor);
         $this->entityManager->flush();
-    }
-
-    /**
-     * @Given There is a :ifVerified vendor which requested change
-     */
-    public function thereIsAVendorWhichRequestedChange($ifVerified)
-    {
-        $vendor = new Vendor();
-        $vendor->setCompanyName('vendor');
-        $vendor->setTaxIdentifier('vendorTax');
-        $vendor->setPhoneNumber('vendorPhone');
-        $vendor->setStatus($ifVerified);
-        $vendor->setEditDate('editDate');
-        $this->entityManager->persist($vendor);
-        $this->entityManager->flush();
-    }
-
-    /**
-     * @When I click :buttonText
-     */
-    public function iClick($buttonText)
-    {
-        $this->getPage()->pressButton($buttonText);
-    }
-
-    /**
-     * @return DocumentElement
-     */
-    private function getPage()
-    {
-        return $this->getSession()->getPage();
     }
 }
