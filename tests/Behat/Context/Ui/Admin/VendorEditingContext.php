c674c19b2852b79265d65c59fc0a3c83f25e0acf
Marcin Kukliński <senghe@gmail.com>
2022-06-23 22:59:31 +0200

    Add OS header & declare statement into missing files

--- tests/Behat/Context/Ui/Admin/VendorEditingContext.php
+++ tests/Behat/Context/Ui/Admin/VendorEditingContext.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Behat\Context\Ui\Admin;
@@ -20,9 +27,9 @@ final class VendorEditingContext extends RawMinkContext implements Context
     }
 
     /**
-     * @Given There is a :ifVerified vendor which :ifRequested change
+     * @Given There is a :ifVerified Vendor who :ifRequested change
      */
-    public function thereIsAVendorWhichChange($ifVerified, $ifRequested)
+    public function thereIsAVendorWhoChange($ifVerified, $ifRequested)
     {
         $vendor = new Vendor();
         $vendor->setCompanyName('vendor');
