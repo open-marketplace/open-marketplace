9564ab28e78bc0b9a0096e280f2e0def7863c446
Rafał Malenta <105425505+rafalmal@users.noreply.github.com>
2022-07-28 14:43:34 +0200

    Update src/Form/Type/Filter/ChoiceFilterType.php
    Co-authored-by: Marcin Kukliński <senghe@gmail.com>

--- src/Form/Type/Filter/ChoiceFilterType.php
+++ src/Form/Type/Filter/ChoiceFilterType.php
@@ -16,8 +16,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
-
 class ChoiceFilterType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver): void
