46d609d138dc93d408c2be75b6296edd20342c80
Raf <rafal.malenta@bitbag.pl>
2022-08-02 12:03:55 +0200

    created public alias for factory

--- tests/Integration/Updater/VendorProfileUpdaterTest.php
+++ tests/Integration/Updater/VendorProfileUpdaterTest.php
@@ -52,8 +52,8 @@ class VendorProfileUpdaterTest extends JsonApiTestCase
         $this->vendorRepository = $this->entityManager->getRepository(Vendor::class);
         $this->vendorProfileUpdateRepository = $this->entityManager->getRepository(VendorProfileUpdate::class);
         $this->vendorProfileUpdateRepository = $this->entityManager->getRepository(VendorProfileUpdate::class);
-
-        $this->vendorAddressFactory = static::$container->get('bitbag_mvm_plugin.factory.vendor_address_factory');
+//        $this->vendorAddressFactory = $this->getContainer()->get('bitbag_mvm_plugin.factory.vendor_address_factory');
+        $this->vendorAddressFactory = static::$container->get('test.bitbag_mvm_plugin.factory.vendor_address_factory');
         $this->vendorProfileFactory = static::$container->get('bitbag_mvm_plugin.factory.vendor_profile_factory');
 
         $remover = static::$container->get('bitbag_mvm_plugin.remover.profile_update_remover');
