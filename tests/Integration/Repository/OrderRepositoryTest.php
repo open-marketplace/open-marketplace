1ee05bc1db58f6979caf334f8e5168ecef4d6307
Raf <rafal.malenta@bitbag.pl>
2022-08-03 10:05:12 +0200

    Added order repository test

--- tests/Integration/Repository/OrderRepositoryTest.php
+++ tests/Integration/Repository/OrderRepositoryTest.php
@@ -12,26 +12,27 @@ declare(strict_types=1);
 namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Integration\Repository;
 
 use ApiTestCase\JsonApiTestCase;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\Order;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\Vendor;
-use Sylius\Component\Core\Model\Customer;
 
-final class CustomerRepositoryTest extends JsonApiTestCase
+final class OrderRepositoryTest extends JsonApiTestCase
 {
     public function setUp(): void
     {
         parent::setUp();
         $this->entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $this->repository = $this->entityManager->getRepository(Customer::class);
+        $this->repository = $this->entityManager->getRepository(Order::class);
     }
 
     public function test_it_finds_all_customers_of_vendor(): void
     {
-        $this->loadFixturesFromFile('CustomerRepositoryTest/test_it_finds_all_customers_of_vendor.yml');
+        $this->loadFixturesFromFile('OrderRepositoryTest/test_it_finds_all_vendor_orders.yml');
 
         $vendorOliver = $this->entityManager->getRepository(Vendor::class)->findOneBy(['slug'=>'oliver-queen-company']);
-        $queryBuilder = $this->repository->findVendorCustomers($vendorOliver);
+        $queryBuilder = $this->repository->findAllByVendor($vendorOliver);
 
         $result = $queryBuilder->getQuery()->getResult();
+//        dd($result);
         self::assertEquals(1, count($result));
     }
 }
