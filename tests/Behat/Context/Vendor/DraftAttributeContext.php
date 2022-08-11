41b7a8d08ff1cb68e06a618e1cff045ef2f1a3fa
Raf <rafal.malenta@bitbag.pl>
2022-08-11 08:25:30 +0200

    Added behat scenario

---
+++ tests/Behat/Context/Vendor/DraftAttributeContext.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\BitBag\SyliusMultiVendorMarketplacePlugin\Behat\Context\Vendor;
+
+use Behat\MinkExtension\Context\RawMinkContext;
+use function PHPUnit\Framework\assertTrue;
+
+final class DraftAttributeContext extends RawMinkContext
+{
+    /**
+     * @When I fill form with :code and name with :name and submit
+     */
+    public function iFillCodeWithAndNameWith($code, $name)
+    {
+        $page = $this->getSession()->getPage();
+        $codeInput = $page->find('css', '#sylius_product_attribute_code');
+        $codeInput->setValue($code);
+
+        $nameInput = $page->find('css', '#sylius_product_attribute_translations_en_US_name');
+        $nameInput->setValue($name);
+
+        $submitButton = $page->find('css','.ui.labeled.icon.primary.button');
+        $submitButton->press();
+    }
+
+    /**
+     * @Then I should see attribute with :arg1 and :arg2 type :type
+     */
+    public function iShouldSeeAttributeWithAnd($code, $name, $type)
+    {
+        $page = $this->getSession()->getPage();
+        $gridTable = $page->find('css','.ui.sortable.stackable.very.basic.celled.table');
+        $rows = $gridTable->findAll('css','.item');
+        foreach ($rows as $row){
+            if(
+                str_contains($row->getText(), $code) &&
+                str_contains($row->getText(), $name) &&
+                str_contains($row->getText(), $type)
+            ) {
+                $rowWithValueExist = true;
+            }
+        }
+
+        assertTrue($rowWithValueExist);
+    }
+
+
+}
