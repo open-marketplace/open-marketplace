a965aec66386f532938b2cd058360524f409d958
Raf <rafal.malenta@bitbag.pl>
2022-08-29 08:08:39 +0200

    Added spec scenario

--- spec/Factory/DraftAttributeFactorySpec.php
+++ spec/Factory/DraftAttributeFactorySpec.php
@@ -11,9 +11,13 @@ declare(strict_types=1);
 
 namespace spec\BitBag\SyliusMultiVendorMarketplacePlugin\Factory;
 
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\ProductListing\DraftAttributeInterface;
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\VendorInterface;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Factory\DraftAttributeFactory;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Provider\VendorProviderInterface;
 use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\AttributeBundle\Form\Type\AttributeType;
+use Sylius\Component\Attribute\AttributeType\AttributeTypeInterface;
 use Sylius\Component\Registry\ServiceRegistryInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 
@@ -32,8 +36,30 @@ final class DraftAttributeFactorySpec extends ObjectBehavior
         $this->shouldHaveType(DraftAttributeFactory::class);
     }
 
-    function it_create_typed_attribute(): void
-    {
+    function it_create_typed_attribute(
+        ServiceRegistryInterface $attributeTypesRegistry,
+        AttributeTypeInterface $attributeType,
+        VendorProviderInterface $vendorProvider,
+        FactoryInterface $factory,
+        DraftAttributeInterface $attribute,
+        DraftAttributeInterface $typedAttribute,
+        VendorInterface $vendor
+    ): void {
+        $type = 'text';
+        $storageType = 'text';
 
+        $attributeTypesRegistry->get($type)->willReturn($attributeType);
+        $attributeType->getStorageType()->willReturn($storageType);
+        $factory->createNew()->willReturn($attribute);
+        $vendorProvider->provideCurrentVendor()->willReturn($vendor);
+
+        $attribute->setType($type)->shouldBeCalledOnce();
+        $attribute->setStorageType($storageType)->shouldBeCalledOnce();
+        $attribute->setVendor($vendor)->shouldBeCalledOnce();
+
+
+        $item = $this->createTyped($type);
+
+        $item->shouldBeAnInstanceOf(DraftAttributeInterface::class);
     }
 }
