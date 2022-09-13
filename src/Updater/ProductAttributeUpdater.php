ac3ff343631bc3c0e2bc4f8fb3f202ec8eddfff1
Raf <rafal.malenta@bitbag.pl>
2022-09-13 12:22:39 +0200

    Finished ProductAttributeUpdater

--- src/Updater/ProductAttributeUpdater.php
+++ src/Updater/ProductAttributeUpdater.php
@@ -11,7 +11,38 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Updater;
 
-class ProductAttributeUpdater
-{
+use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\ProductListing\DraftAttributeInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Attribute\Factory\AttributeFactory;
+use Sylius\Component\Product\Model\ProductAttributeInterface;
+use Sylius\Component\Product\Model\ProductAttributeTranslation;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
+final class ProductAttributeUpdater
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function update(DraftAttributeInterface $draftAttribute, ProductAttributeInterface $productAttribute): void
+    {
+        $productAttribute->setPosition($productAttribute->getPosition());
+
+        $draftTranslations = $draftAttribute->getTranslations();
+
+        foreach ($productAttribute->getTranslations() as $translation) {
+            $this->entityManager->remove($translation);
+        }
+
+        foreach ($draftTranslations as $draftTranslation) {
+            $productAttributeTranslation = new ProductAttributeTranslation();
+            $productAttributeTranslation->setTranslatable($productAttribute);
+            $productAttributeTranslation->setLocale($draftTranslation->getLocale());
+            $productAttributeTranslation->setName($draftTranslation->getName());
+            $this->entityManager->persist($productAttributeTranslation);
+        }
+    }
 }
