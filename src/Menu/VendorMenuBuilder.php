13020e908ba50c32498f3ad394c0cbaa9c63d901
Wojtek <wojtek.przygoda@gmail.com>
2022-09-26 08:22:33 +0200

    change items order for vendor menu

--- src/Menu/VendorMenuBuilder.php
+++ src/Menu/VendorMenuBuilder.php
@@ -53,35 +53,6 @@ final class VendorMenuBuilder
                 ->setLabel('bitbag_mvm_plugin.ui.become_a_vendor')
                 ->setLabelAttribute('icon', 'star');
         } else {
-            $menu
-                ->addChild('newes', ['route' => 'vendor_profile'])
-                ->setLabel('bitbag_mvm_plugin.ui.vendor_profile')
-                ->setLabelAttribute('icon', 'pencil');
-
-            $menu
-                ->addChild('conversations', ['route' => 'bitbag_mvm_plugin_vendor_conversation_index'])
-                ->setLabel('bitbag_mvm_plugin.ui.menu.conversations')
-                ->setLabelAttribute('icon', 'envelope open');
-
-            $menu
-                ->addChild('Product List', ['route' => 'bitbag_mvm_plugin_vendor_product_listing_index'])
-                ->setLabel('bitbag_mvm_plugin.ui.product_list')
-                ->setLabelAttribute('icon', 'inbox');
-
-            $menu
-                ->addChild('Order List', ['route' => 'bitbag_mvm_plugin_order_listing'])
-                ->setLabel('bitbag_mvm_plugin.ui.order_list')
-                ->setLabelAttribute('icon', 'suitcase');
-
-            $menu
-                ->addChild('Clients', ['route' => 'bitbag_mvm_plugin_customer_index'])
-                ->setLabel('bitbag_mvm_plugin.ui.clients')
-                ->setLabelAttribute('icon', 'users');
-
-            $menu
-                ->addChild('Inventory', ['route' => 'vendor_product_variant_index'])
-                ->setLabel('bitbag_mvm_plugin.ui.inventory')
-                ->setLabelAttribute('icon', 'clipboard');
 
             $menu
                 ->addChild('Attributes', ['route' => 'bitbag_mvm_vendor_draft_attribute_index'])
@@ -89,9 +60,38 @@ final class VendorMenuBuilder
                 ->setLabelAttribute('icon', 'clipboard');
 
             $menu
+                ->addChild('Product List', ['route' => 'bitbag_mvm_plugin_vendor_product_listing_index'])
+                ->setLabel('bitbag_mvm_plugin.ui.product_list')
+                ->setLabelAttribute('icon', 'inbox');
+
+            $menu
+                ->addChild('Inventory', ['route' => 'vendor_product_variant_index'])
+                ->setLabel('bitbag_mvm_plugin.ui.inventory')
+                ->setLabelAttribute('icon', 'clipboard');
+
+            $menu
+                ->addChild('Order List', ['route' => 'bitbag_mvm_plugin_order_listing'])
+                ->setLabel('bitbag_mvm_plugin.ui.order_list')
+                ->setLabelAttribute('icon', 'suitcase');
+            
+            $menu
+                ->addChild('Clients', ['route' => 'bitbag_mvm_plugin_customer_index'])
+                ->setLabel('bitbag_mvm_plugin.ui.clients')
+                ->setLabelAttribute('icon', 'users');
+
+            $menu
                 ->addChild('Shipping', ['route' => 'vendor_shipping_methods'])
                 ->setLabel('bitbag_mvm_plugin.ui.shipping_methods')
                 ->setLabelAttribute('icon', 'shipping');
+
+            $menu
+                ->addChild('conversations', ['route' => 'bitbag_mvm_plugin_vendor_conversation_index'])
+                ->setLabel('bitbag_mvm_plugin.ui.menu.conversations')
+                ->setLabelAttribute('icon', 'envelope open');
+            $menu
+                ->addChild('newes', ['route' => 'vendor_profile'])
+                ->setLabel('bitbag_mvm_plugin.ui.vendor_profile')
+                ->setLabelAttribute('icon', 'pencil');
         }
 
         $this->eventDispatcher->dispatch(new MenuBuilderEvent($this->factory, $menu), self::EVENT_NAME);
