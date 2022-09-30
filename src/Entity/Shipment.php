68798704ac487681820618432c6b86598a372fcf
Raf <rafal.malenta@bitbag.pl>
2022-09-30 12:11:32 +0200

    Fixed mailer

--- src/Entity/Shipment.php
+++ src/Entity/Shipment.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusMultiVendorMarketplacePlugin\Entity;
 
-use BitBag\SyliusMultiVendorMarketplacePlugin\Entity\ShipmentInterface;
 use BitBag\SyliusMultiVendorMarketplacePlugin\Model\Shipment\ShipmentTrait;
 use Sylius\Component\Core\Model\Shipment as BaseShipment;
 
