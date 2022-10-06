cbece44e204c49721e8a00a8b31c17d226ab8b0b
liszkapawel <pawel.liszka00@gmail.com>
2022-10-06 12:36:38 +0200

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
+const [bitbagCmsShop, bitbagCmsAdmin] = require('./vendor/bitbag/cms-plugin/webpack.config.js');
+
 const syliusBundles = path.resolve(__dirname, 'vendor/sylius/sylius/src/Sylius/Bundle/');
 const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
 const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
@@ -44,4 +46,4 @@ adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 adminConfig.externals = Object.assign({}, adminConfig.externals, { window: 'window', document: 'document' });
 adminConfig.name = 'admin';
 
-module.exports = [shopConfig, adminConfig];
+module.exports = [shopConfig, adminConfig, bitbagCmsShop, bitbagCmsAdmin];
