e002f55d05410dd78070a7c20a9336e74cb3af5f
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-13 12:58:10 +0100

    Decorated OrderInventoryOperator.php

---
+++ src/Operator/OrderInventoryOperator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BitBag\OpenMarketplace\Operator;
+
+use BitBag\OpenMarketplace\Entity\OrderInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Core\Inventory\Operator\OrderInventoryOperatorInterface;
+
+
+class OrderInventoryOperator
+{
+    private OrderInventoryOperatorInterface $decoratedOperator;
+
+    private EntityManagerInterface $productVariantManager;
+
+    public function __construct(
+        OrderInventoryOperatorInterface $decoratedOperator,
+        EntityManagerInterface $productVariantManager,
+    ) {
+        $this->decoratedOperator = $decoratedOperator;
+        $this->productVariantManager = $productVariantManager;
+    }
+
+
+    public function cancel(OrderInterface $order): void
+    {
+        if($order->getPrimaryOrder() !== null) {
+            $this->decoratedOperator->cancel($order);
+        }
+    }
+
+    public function hold(OrderInterface $order): void
+    {
+        if($order->getPrimaryOrder() !== null) {
+            $this->decoratedOperator->hold($order);
+        }
+    }
+
+    public function sell(OrderInterface $order): void
+    {
+        if($order->getPrimaryOrder() !== null) {
+            $this->decoratedOperator->sell($order);
+        }
+    }
+}
