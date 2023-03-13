d770dfa907cc0d8031cb0aeac99dc49b514f8539
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-13 13:04:28 +0100

    added final and OS header

--- src/Operator/OrderInventoryOperator.php
+++ src/Operator/OrderInventoryOperator.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\OpenMarketplace\Operator;
@@ -8,7 +15,7 @@ use BitBag\OpenMarketplace\Entity\OrderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Core\Inventory\Operator\OrderInventoryOperatorInterface;
 
-class OrderInventoryOperator
+final class OrderInventoryOperator
 {
     private OrderInventoryOperatorInterface $decoratedOperator;
 
