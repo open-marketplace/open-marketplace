daebe19dcc9fb9e18059412d83fbbc577f773c73
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-13 13:01:47 +0100

    ECS fix

--- src/Operator/OrderInventoryOperator.php
+++ src/Operator/OrderInventoryOperator.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BitBag\OpenMarketplace\Operator;
 
 use BitBag\OpenMarketplace\Entity\OrderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Core\Inventory\Operator\OrderInventoryOperatorInterface;
-
 
 class OrderInventoryOperator
 {
@@ -21,24 +22,23 @@ class OrderInventoryOperator
         $this->productVariantManager = $productVariantManager;
     }
 
-
     public function cancel(OrderInterface $order): void
     {
-        if($order->getPrimaryOrder() !== null) {
+        if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->cancel($order);
         }
     }
 
     public function hold(OrderInterface $order): void
     {
-        if($order->getPrimaryOrder() !== null) {
+        if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->hold($order);
         }
     }
 
     public function sell(OrderInterface $order): void
     {
-        if($order->getPrimaryOrder() !== null) {
+        if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->sell($order);
         }
     }
