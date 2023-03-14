24a01334fc42857bf65c81ef340fb35d187fa2ec
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-14 12:55:27 +0100

    Added guard clause instead phpdocs and removed constructor argumen

--- src/Operator/OrderInventoryOperator.php
+++ src/Operator/OrderInventoryOperator.php
@@ -11,43 +11,45 @@ declare(strict_types=1);
 
 namespace BitBag\OpenMarketplace\Operator;
 
-use Sylius\Component\Core\Model\OrderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Core\Inventory\Operator\OrderInventoryOperatorInterface;
+use Sylius\Component\Core\Model\OrderInterface;
 
 final class OrderInventoryOperator implements OrderInventoryOperatorInterface
 {
     private OrderInventoryOperatorInterface $decoratedOperator;
 
-    private EntityManagerInterface $productVariantManager;
-
     public function __construct(
         OrderInventoryOperatorInterface $decoratedOperator,
-        EntityManagerInterface $productVariantManager,
     ) {
         $this->decoratedOperator = $decoratedOperator;
-        $this->productVariantManager = $productVariantManager;
     }
 
-    /** @param \BitBag\OpenMarketplace\Entity\OrderInterface $order */
     public function cancel(OrderInterface $order): void
     {
+        if (!$order instanceof \BitBag\OpenMarketplace\Entity\OrderInterface) {
+            return;
+        }
         if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->cancel($order);
         }
     }
 
-    /** @param \BitBag\OpenMarketplace\Entity\OrderInterface $order */
     public function hold(OrderInterface $order): void
     {
+        if (!$order instanceof \BitBag\OpenMarketplace\Entity\OrderInterface) {
+            return;
+        }
         if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->hold($order);
         }
     }
 
-    /** @param \BitBag\OpenMarketplace\Entity\OrderInterface $order */
     public function sell(OrderInterface $order): void
     {
+        if (!$order instanceof \BitBag\OpenMarketplace\Entity\OrderInterface) {
+            return;
+        }
         if (null !== $order->getPrimaryOrder()) {
             $this->decoratedOperator->sell($order);
         }
