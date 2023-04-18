b39e118134ee818491b98c10bea3dae39b575aa7
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-04-18 12:56:52 +0200

    Added flush and test

---
+++ tests/Integration/Converter/AttributesConverterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\BitBag\OpenMarketplace\Integration\Converter;
+
+use ApiTestCase\JsonApiTestCase;
+use BitBag\OpenMarketplace\Entity\Product;
+use BitBag\OpenMarketplace\Entity\ProductListing\ProductDraft;
+
+class AttributesConverterTest extends JsonApiTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->entityManager = $this->getEntityManager();
+        $this->attributesConverter = $this->getContainer()->get('open_marketplace.attributes_converter');
+    }
+
+    public function test_it_removes_attributes_from_product(): void
+    {
+        $this->loadFixturesFromFile('AttributesConverterTest/test_it_removes_attributes_from_product.yml');
+        $draft = $this->entityManager->getRepository(ProductDraft::class)->findAll()[0];
+        $product = $draft->getProductListing()->getProduct();
+
+        $this->attributesConverter->convert($draft, $product);
+        $this->entityManager->flush();
+
+        $freshProduct = $this->entityManager->getRepository(Product::class)->findAll()[0];
+        $this->entityManager->refresh($freshProduct);
+
+        $this->assertEquals(0, count($freshProduct->getAttributes()));
+    }
+}
