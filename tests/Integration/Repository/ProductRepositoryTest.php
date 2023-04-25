e6b75cf2c593faab45dc631477b0b868f0c064d8
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-04-25 13:05:25 +0200

    Added integration tests for repositories

---
+++ tests/Integration/Repository/ProductRepositoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\BitBag\OpenMarketplace\Integration\Repository;
+
+use ApiTestCase\JsonApiTestCase;
+use BitBag\OpenMarketplace\Entity\Product;
+use BitBag\OpenMarketplace\Entity\Vendor;
+use BitBag\OpenMarketplace\Entity\VendorInterface;
+use Doctrine\ORM\QueryBuilder;
+use Sylius\Component\Core\Model\Channel;
+
+class ProductRepositoryTest extends JsonApiTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $this->repository = $this->entityManager->getRepository(Product::class);
+        $this->taxonProvider = $this->getContainer()->get('open_marketplace.provider.taxon');
+    }
+
+    public function test_it_finds_vendor_products(): void
+    {
+        $this->loadFixturesFromFile('ProductRepositoryTest/test_it_finds_vendor_products.yml');
+        /** @var VendorInterface $vendorOliver */
+        $vendorOliver = $this->entityManager->getRepository(Vendor::class)->findOneBySlug('oliver-queen-company');
+        $channel = $this->entityManager->getRepository(Channel::class)->findAll()[0];
+        $localeCode = $channel->getDefaultLocale()->getCode();
+        $taxon = $this->taxonProvider->provideForVendorPage(null, $localeCode);
+        /** @var QueryBuilder $vendorProductsQuery */
+        $vendorProductsQuery = $this->repository->createVendorShopListQueryBuilder($vendorOliver, $channel, $taxon, 'en_US', [], true);
+
+        $this->assertCount(2, $vendorProductsQuery->getQuery()->getResult());
+
+    }
+}
---
+++ tests/Integration/Repository/VendorRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\BitBag\OpenMarketplace\Integration\Repository;
+
+use ApiTestCase\JsonApiTestCase;
+use BitBag\OpenMarketplace\Entity\Vendor;
+use BitBag\OpenMarketplace\Entity\VendorInterface;
+
+class VendorRepositoryTest extends JsonApiTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $this->repository = $this->entityManager->getRepository(Vendor::class);
+    }
+
+    public function test_it_finds_correct_vendor(): void
+    {
+        $this->loadFixturesFromFile('VendorRepositoryTest/test_it_finds_correct_vendor.yml');
+        /** @var VendorInterface $vendorOliver */
+        $vendorOliver = $this->entityManager->getRepository(Vendor::class)->findOneBySlug('oliver-queen-company');
+        $vendorBruce = $this->entityManager->getRepository(Vendor::class)->findOneBySlug('bruce-wayne-company');
+
+        $this->assertEquals('Queen company', $vendorOliver->getCompanyName());
+        $this->assertEquals('Wayne enterprise', $vendorBruce->getCompanyName());
+    }
+}
