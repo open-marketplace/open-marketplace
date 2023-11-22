68b7430a0b54776a83b37fa64633563e2ad54044
beniaminblaszkiewicz <blaszkiewiczbeniamin@gmail.com>
2023-11-22 17:36:24 +0100

    MUL-288: clean up SettlementRepositoryTest

--- tests/Integration/Repository/SettlementRepositoryTest.php
+++ tests/Integration/Repository/SettlementRepositoryTest.php
@@ -12,21 +12,19 @@ declare(strict_types=1);
 namespace Tests\BitBag\OpenMarketplace\Integration\Repository;
 
 use ApiTestCase\JsonApiTestCase;
-use BitBag\OpenMarketplace\Component\Vendor\Entity\Vendor;
 
 final class SettlementRepositoryTest extends JsonApiTestCase
 {
     public function setUp(): void
     {
         parent::setUp();
-        $this->entityManager = self::getContainer()->get('doctrine.orm.entity_manager');
         $this->repository = self::getContainer()->get('open_marketplace.repository.settlement');
     }
 
     public function test_it_finds_last_settlement_for_vendor(): void
     {
         $this->loadFixturesFromFile('SettlementRepositoryTest/test_it_find_last_settlement_for_vendor.yml');
-        $vendorRepository = $this->getEntityManager()->getRepository(Vendor::class);
+        $vendorRepository = self::getContainer()->get('open_marketplace.repository.vendor');
         $vendor = $vendorRepository->findOneBy(['slug' => 'Wayne-Enterprises-Inc']);
 
         $settlement = $this->repository->findLastByVendor($vendor);
