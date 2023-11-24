89ae35120b680bde09ee9376159986f23791b6c4
beniaminblaszkiewicz <blaszkiewiczbeniamin@gmail.com>
2023-11-24 11:45:21 +0100

    MUL-288: remove settlementdto and fix settlement factory

--- src/Component/Settlement/Factory/SettlementFactory.php
+++ src/Component/Settlement/Factory/SettlementFactory.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace BitBag\OpenMarketplace\Component\Settlement\Factory;
 
-use BitBag\OpenMarketplace\Component\Settlement\DTO\SettlementDTO;
 use BitBag\OpenMarketplace\Component\Settlement\Entity\Settlement;
 use BitBag\OpenMarketplace\Component\Settlement\Entity\SettlementInterface;
 use BitBag\OpenMarketplace\Component\Vendor\Entity\VendorInterface;
@@ -22,18 +21,6 @@ final class SettlementFactory implements SettlementFactoryInterface
     public function createNew(): SettlementInterface
     {
         return new Settlement();
-    }
-
-    public function createNewFromDTOAndVendor(SettlementDTO $settlementDTO, VendorInterface $vendor): SettlementInterface
-    {
-        $settlement = $this->createNew();
-        $settlement->setVendor($vendor);
-        $settlement->setStartDate($settlementDTO->getStartDate());
-        $settlement->setEndDate($settlementDTO->getEndDate());
-        $settlement->setTotalAmount($settlementDTO->getTotalAmount());
-        $settlement->setTotalCommissionAmount($settlementDTO->getTotalCommissionAmount());
-
-        return $settlement;
     }
 
     public function createNewForVendorAndChannel(
