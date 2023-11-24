e7480b430b1168c4e9aa754c1ad1e60571c7846f
beniaminblaszkiewicz <blaszkiewiczbeniamin@gmail.com>
2023-11-24 11:41:59 +0100

    MUL-288: fix settlement repository test

--- tests/Integration/Repository/SettlementRepositoryTest.php
+++ tests/Integration/Repository/SettlementRepositoryTest.php
@@ -21,16 +21,17 @@ final class SettlementRepositoryTest extends JsonApiTestCase
         $this->repository = self::getContainer()->get('open_marketplace.repository.settlement');
     }
 
-//    public function test_it_finds_last_settlement_for_vendor(): void
-//    {
-//        $this->loadFixturesFromFile('SettlementRepositoryTest/test_it_find_last_settlement_for_vendor.yaml');
-//        $vendorRepository = self::getContainer()->get('open_marketplace.repository.vendor');
-//        $vendor = $vendorRepository->findOneBy(['slug' => 'Wayne-Enterprises-Inc']);
-//
-//        $settlement = $this->repository->findLastByVendor($vendor);
-//
-//        $this->assertSame('USD', $settlement->getCurrencyCode());
-//        $this->assertSame(10000, $settlement->getTotalAmount());
-//        $this->assertSame(100, $settlement->getTotalCommissionAmount());
-//    }
+    public function test_it_finds_last_settlement_for_vendor(): void
+    {
+        $this->loadFixturesFromFile('SettlementRepositoryTest/test_it_find_last_settlement_for_vendor.yaml');
+        $vendorRepository = self::getContainer()->get('open_marketplace.repository.vendor');
+        $channelRepository = self::getContainer()->get('sylius.repository.channel');
+        $vendor = $vendorRepository->findOneBy(['slug' => 'Wayne-Enterprises-Inc']);
+        $channel = $channelRepository->findOneBy(['code' => 'US']);
+
+        $settlement = $this->repository->findLastByVendorAndChannel($vendor, $channel);
+
+        $this->assertSame(10000, $settlement->getTotalAmount());
+        $this->assertSame(100, $settlement->getTotalCommissionAmount());
+    }
 }
