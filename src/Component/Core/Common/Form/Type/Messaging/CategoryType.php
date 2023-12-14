67e2f04c6d3d65bb7de48504e20ee883074ab7a1
mikolaj.klosowski <mklosowski@notebooksbilliger.de>
2023-12-14 12:30:41 +0100

    [MUL-277] Add test to cover validation of category message

--- src/Component/Core/Common/Form/Type/Messaging/CategoryType.php
+++ src/Component/Core/Common/Form/Type/Messaging/CategoryType.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
 declare(strict_types=1);
 
 namespace BitBag\OpenMarketplace\Component\Core\Common\Form\Type\Messaging;
@@ -10,7 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CategoryType extends AbstractType
+final class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -30,6 +37,6 @@ class CategoryType extends AbstractType
 
     public function getBlockPrefix(): string
     {
-        return 'mvm_category';
+        return 'open_marketplace_category';
     }
 }
