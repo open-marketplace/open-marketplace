e06997279e2eaad6b54c610410ba62bde10b4750
mikolaj.klosowski <mklosowski@notebooksbilliger.de>
2023-12-27 15:22:10 +0100

    [MUL-245] Create working solution to fix this bug

---
+++ src/Component/ProductListing/Repository/DraftImageRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file has been created by developers from BitBag.
+ * Feel free to contact us once you face any issues or want to start
+ * You can find more information about us on https://bitbag.io and write us
+ * an email on hello@bitbag.io.
+ */
+
+namespace BitBag\OpenMarketplace\Component\ProductListing\Repository;
+
+use BitBag\OpenMarketplace\Component\ProductListing\Entity\DraftInterface;
+use Doctrine\ORM\QueryBuilder;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class DraftImageRepository extends EntityRepository implements DraftImageRepositoryInterface
+{
+    public function findVendorDraftImages(DraftInterface $draft): array
+    {
+        $queryBuilder = $this->findVendorDraftImagesQuery($draft);
+
+        return $queryBuilder
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
+    public function findVendorDraftImagesQuery(DraftInterface $draft): QueryBuilder
+    {
+        $draftId = $draft->getId();
+
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.owner = :draft')
+            ->setParameter('draft', $draftId)
+            ;
+    }
+}
