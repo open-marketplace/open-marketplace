3d6e48da002c9df3106e7034eb4090ca75cb39c3
Marcin Kukliński <senghe@gmail.com>
2024-01-17 19:31:45 +0100

    Fix ECS issues

--- src/Component/Core/Settlement/Form/ProfitWithdrawalType.php
+++ src/Component/Core/Settlement/Form/ProfitWithdrawalType.php
@@ -12,10 +12,10 @@ declare(strict_types=1);
 namespace BitBag\OpenMarketplace\Component\Core\Settlement\Form;
 
 use BitBag\OpenMarketplace\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Bundle\MoneyBundle\Form\Type\MoneyType;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Currency\Model\CurrencyInterface;
 use Symfony\Component\Form\AbstractType;
-use Sylius\Bundle\MoneyBundle\Form\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
