adb50785af68ef6c81c8bc7dcaf20b18f3ada98c
Marek Rzytki <rzytki7@gmail.com>
2024-01-23 11:22:42 +0100

    Resolve doctrine extension logic from base Sylius class

--- spec/Component/Core/Api/Doctrine/QueryItemExtension/OrderGetMethodItemExtensionSpec.php
+++ spec/Component/Core/Api/Doctrine/QueryItemExtension/OrderGetMethodItemExtensionSpec.php
@@ -10,8 +10,8 @@ declare(strict_types=1);
 
 namespace spec\BitBag\OpenMarketplace\Component\Core\Api\Doctrine\QueryItemExtension;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface as LegacyQueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use BitBag\OpenMarketplace\Component\Core\Api\Doctrine\QueryItemExtension\OrderGetMethodItemExtension;
 use BitBag\OpenMarketplace\Component\Core\Api\SectionResolver\ShopVendorApiSection;
 use BitBag\OpenMarketplace\Component\Order\Entity\OrderInterface;
@@ -24,15 +24,15 @@ use Sylius\Bundle\ApiBundle\Context\UserContextInterface;
 use Sylius\Bundle\ApiBundle\SectionResolver\ShopApiSection;
 use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
 use Sylius\Component\Core\Model\AdminUserInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
 
 final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
 {
     public function let(
-        QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         SectionProviderInterface $sectionProvider,
         UserContextInterface $userContext
     ): void {
-        $this->beConstructedWith($baseOrderGetMethodItemExtension, $sectionProvider, $userContext);
+        $this->beConstructedWith($sectionProvider, $userContext);
     }
 
     public function it_is_initializable(): void
@@ -42,10 +42,9 @@ final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
     }
 
     public function it_does_not_filter_for_not_supported_class(
-        QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         SectionProviderInterface $sectionProvider,
         QueryBuilder $queryBuilder,
-        LegacyQueryNameGeneratorInterface $queryNameGenerator
+        QueryNameGeneratorInterface $queryNameGenerator
     ): void {
         $this->applyToItem(
             $queryBuilder,
@@ -55,22 +54,13 @@ final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
         );
 
         $sectionProvider->getSection()->shouldNotHaveBeenCalled();
-        $baseOrderGetMethodItemExtension->applyToItem(
-            $queryBuilder,
-            $queryNameGenerator,
-            OrderInterface::class,
-            ['id'],
-            null,
-            []
-        )->shouldNotHaveBeenCalled();
     }
 
     public function it_does_not_filter_if_shop_vendor_api_section(
-        QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         SectionProviderInterface $sectionProvider,
         ShopVendorApiSection $shopVendorApiSection,
         QueryBuilder $queryBuilder,
-        LegacyQueryNameGeneratorInterface $queryNameGenerator
+        QueryNameGeneratorInterface $queryNameGenerator
     ): void {
         $sectionProvider->getSection()->willReturn($shopVendorApiSection);
 
@@ -80,28 +70,20 @@ final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
             OrderInterface::class,
             ['id']
         );
-
-        $baseOrderGetMethodItemExtension->applyToItem(
-            $queryBuilder,
-            $queryNameGenerator,
-            OrderInterface::class,
-            ['id'],
-            null,
-            []
-        )->shouldNotHaveBeenCalled();
     }
 
     public function it_filters_if_not_shop_vendor_api_section_and_it_is_logged_in_admin_user(
-        QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         SectionProviderInterface $sectionProvider,
         ShopApiSection $shopApiSection,
         UserContextInterface $userContext,
         AdminUserInterface $user,
         QueryBuilder $queryBuilder,
-        LegacyQueryNameGeneratorInterface $queryNameGenerator
+        QueryNameGeneratorInterface $queryNameGenerator
     ): void {
         $sectionProvider->getSection()->willReturn($shopApiSection);
         $userContext->getUser()->willReturn($user);
+        $queryBuilder->getRootAliases()->willReturn(['root']);
+        $user->getRoles()->willReturn(['ROLE_API_ACCESS']);
 
         $this->applyToItem(
             $queryBuilder,
@@ -110,31 +92,52 @@ final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
             ['id']
         );
 
-        $queryBuilder->getRootAliases()->shouldNotHaveBeenCalled();
         $queryBuilder->andWhere(Argument::any())->shouldNotHaveBeenCalled();
-        $baseOrderGetMethodItemExtension->applyToItem(
-            $queryBuilder,
-            $queryNameGenerator,
-            OrderInterface::class,
-            ['id'],
-            null,
-            []
-        )->shouldHaveBeenCalled();
     }
 
     public function it_filters_if_not_shop_vendor_api_section_and_it_is_logged_in_shop_user(
-        QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         SectionProviderInterface $sectionProvider,
         ShopApiSection $shopApiSection,
         UserContextInterface $userContext,
         ShopUserInterface $user,
         QueryBuilder $queryBuilder,
-        LegacyQueryNameGeneratorInterface $queryNameGenerator
+        QueryNameGeneratorInterface $queryNameGenerator,
+        CustomerInterface $customer
     ): void {
         $sectionProvider->getSection()->willReturn($shopApiSection);
-        $userContext->getUser()->willReturn($user);
+        $queryNameGenerator->generateParameterName('customer')->shouldBeCalled()->willReturn('customer');
+
         $queryBuilder->getRootAliases()->willReturn(['root']);
         $queryBuilder->andWhere(Argument::any())->willReturn($queryBuilder);
+
+        $userContext->getUser()->willReturn($user);
+        $user->getCustomer()->willReturn($customer);
+        $customer->getId()->willReturn(1);
+        $user->getRoles()->willReturn(['ROLE_USER']);
+
+        $queryBuilder
+            ->andWhere('root.mode != :primaryMode')
+            ->shouldBeCalled()
+            ->willReturn($queryBuilder)
+        ;
+
+        $queryBuilder
+            ->setParameter('primaryMode', OrderInterface::PRIMARY_ORDER_MODE)
+            ->shouldBeCalled()
+            ->willReturn($queryBuilder)
+        ;
+
+        $queryBuilder
+            ->andWhere(sprintf('%s.customer = :customer', 'root'))
+            ->shouldBeCalled()
+            ->willReturn($queryBuilder)
+        ;
+
+        $queryBuilder
+            ->setParameter('customer', 1)
+            ->shouldBeCalled()
+            ->willReturn($queryBuilder)
+        ;
 
         $this->applyToItem(
             $queryBuilder,
@@ -142,17 +145,5 @@ final class OrderGetMethodItemExtensionSpec extends ObjectBehavior
             OrderInterface::class,
             ['id']
         );
-
-        $queryBuilder->andWhere('root.mode != :primaryMode')->shouldHaveBeenCalled();
-        $queryBuilder->setParameter('primaryMode', OrderInterface::PRIMARY_ORDER_MODE)->shouldHaveBeenCalled();
-
-        $baseOrderGetMethodItemExtension->applyToItem(
-            $queryBuilder,
-            $queryNameGenerator,
-            OrderInterface::class,
-            ['id'],
-            null,
-            []
-        )->shouldHaveBeenCalled();
     }
 }
--- src/Component/Core/Api/Doctrine/QueryItemExtension/OrderGetMethodItemExtension.php
+++ src/Component/Core/Api/Doctrine/QueryItemExtension/OrderGetMethodItemExtension.php
@@ -10,19 +10,23 @@ declare(strict_types=1);
 
 namespace BitBag\OpenMarketplace\Component\Core\Api\Doctrine\QueryItemExtension;
 
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface as LegacyQueryNameGeneratorInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
 use BitBag\OpenMarketplace\Component\Core\Api\SectionResolver\ShopVendorApiSection;
 use BitBag\OpenMarketplace\Component\Vendor\Entity\ShopUserInterface;
 use Doctrine\ORM\QueryBuilder;
 use Sylius\Bundle\ApiBundle\Context\UserContextInterface;
 use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Component\Core\Model\AdminUserInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 final class OrderGetMethodItemExtension implements QueryItemExtensionInterface
 {
     public function __construct(
-        private QueryItemExtensionInterface $baseOrderGetMethodItemExtension,
         private SectionProviderInterface $sectionProvider,
         private UserContextInterface $userContext
     ) {
@@ -30,10 +34,10 @@ final class OrderGetMethodItemExtension implements QueryItemExtensionInterface
 
     public function applyToItem(
         QueryBuilder $queryBuilder,
-        LegacyQueryNameGeneratorInterface $queryNameGenerator,
+        QueryNameGeneratorInterface $queryNameGenerator,
         string $resourceClass,
         array $identifiers,
-        string $operationName = null,
+        Operation $operation = null,
         array $context = []
     ): void {
         if (!is_a($resourceClass, OrderInterface::class, true)) {
@@ -44,21 +48,64 @@ final class OrderGetMethodItemExtension implements QueryItemExtensionInterface
             return;
         }
 
-        if ($this->userContext->getUser() instanceof ShopUserInterface) {
-            $rootAlias = $queryBuilder->getRootAliases()[0];
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $user = $this->userContext->getUser();
+
+        if ($user instanceof ShopUserInterface) {
             $queryBuilder
                 ->andWhere(sprintf('%s.mode != :primaryMode', $rootAlias))
                 ->setParameter('primaryMode', \BitBag\OpenMarketplace\Component\Order\Entity\OrderInterface::PRIMARY_ORDER_MODE)
             ;
         }
 
-        $this->baseOrderGetMethodItemExtension->applyToItem(
-            $queryBuilder,
-            $queryNameGenerator,
-            $resourceClass,
-            $identifiers,
-            $operationName,
-            $context
-        );
+        $this->applyToItemForGetMethod($user, $queryBuilder, $queryNameGenerator, $rootAlias);
+    }
+
+    private function applyToItemForGetMethod(
+        ?UserInterface $user,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $rootAlias,
+        ): void {
+        if (null === $user) {
+            $queryBuilder
+                ->leftJoin(sprintf('%s.customer', $rootAlias), 'customer')
+                ->leftJoin('customer.user', 'user')
+                ->andWhere($queryBuilder->expr()->orX(
+                    'user IS NULL',
+                    sprintf('%s.customer IS NULL', $rootAlias),
+                    $queryBuilder->expr()->andX(
+                        sprintf('%s.customer IS NOT NULL', $rootAlias),
+                        sprintf('%s.createdByGuest = true', $rootAlias),
+                    ),
+                ))
+            ;
+
+            return;
+        }
+
+        if ($user instanceof ShopUserInterface && in_array('ROLE_USER', $user->getRoles(), true)) {
+            $customerParameterName = $queryNameGenerator->generateParameterName('customer');
+            /** @var ?CustomerInterface $customer */
+            $customer = $user->getCustomer();
+
+            if (null === $customer) {
+                return;
+            }
+
+            $queryBuilder
+                ->andWhere(sprintf('%s.customer = :%s', $rootAlias, $customerParameterName))
+                ->setParameter($customerParameterName, $customer->getId())
+            ;
+
+            return;
+        }
+
+        if ($user instanceof AdminUserInterface && in_array('ROLE_API_ACCESS', $user->getRoles(), true)) {
+            //admin has access to get all orders
+            return;
+        }
+
+        throw new AccessDeniedHttpException('Requested method is not allowed.');
     }
 }
