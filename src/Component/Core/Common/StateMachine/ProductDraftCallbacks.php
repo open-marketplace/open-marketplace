dcb07b2c4456fe6c677f4c8da995a36c47ac8b4b
Marek Rzytki <rzytki7@gmail.com>
2024-01-23 12:55:00 +0100

    Move flashbag from callback service to controllers

--- src/Component/Core/Common/StateMachine/ProductDraftCallbacks.php
+++ src/Component/Core/Common/StateMachine/ProductDraftCallbacks.php
@@ -14,14 +14,10 @@ namespace BitBag\OpenMarketplace\Component\Core\Common\StateMachine;
 use BitBag\OpenMarketplace\Component\ProductListing\DraftConverterInterface;
 use BitBag\OpenMarketplace\Component\ProductListing\Entity\DraftInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 final class ProductDraftCallbacks
 {
     public function __construct(
-        private RequestStack $requestStack,
         private DraftConverterInterface $productDraftService,
         private EntityManagerInterface $entityManager
     ) {
@@ -30,41 +26,24 @@ final class ProductDraftCallbacks
     public function sendToVerification(DraftInterface $productDraft): void
     {
         $productListing = $productDraft->getProductListing();
-        $flashBag = $this->getFlashBag();
 
         $productListing->sendToVerification($productDraft);
         $this->entityManager->flush();
-
-        $flashBag->add('warning', 'open_marketplace.ui.product_listing_sent_to_verification');
     }
 
     public function accept(DraftInterface $productDraft): void
     {
         $product = $this->productDraftService->convertToSimpleProduct($productDraft);
-        $flashBag = $this->getFlashBag();
 
         $this->entityManager->persist($product);
         $this->entityManager->flush();
-
-        $flashBag->add('success', 'open_marketplace.ui.product_listing_accepted');
     }
 
     public function reject(DraftInterface $productDraft): void
     {
         $productListing = $productDraft->getProductListing();
-        $flashBag = $this->getFlashBag();
 
         $productListing->reject();
         $this->entityManager->flush();
-
-        $flashBag->add('warning', 'open_marketplace.ui.product_listing_rejected');
-    }
-
-    private function getFlashBag(): FlashBagInterface
-    {
-        /** @var Session $session */
-        $session = $this->requestStack->getSession();
-
-        return $session->getFlashBag();
     }
 }
